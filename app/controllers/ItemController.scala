5354c0d80684a3282dd6498a0bd104857dfb13cf
ayub <ayusuf@academytrainee.com>
2021-02-10 11:42:50 +0000

    AY: edit

--- app/controllers/ItemController.scala
+++ app/controllers/ItemController.scala
@@ -1,18 +1,33 @@
 package controllers
-import Persistence.Domain.{Items, Item}
+import Persistence.Domain.{Item, Items}
 import Persistence.DAO.ItemDAO
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future._
 import scala.util.{Failure, Success}
 
 class ItemController {
 
   def readAll(): Unit = {
-
+    val readAllFuture = ItemDAO.readAll()
+    readAllFuture.onComplete{
+      case Success(value) =>
+        print("\n")
+        value.foreach(println)
+      case Failure(error) =>
+        error.printStackTrace()
+    }
   }
 
-  def update(): Unit = {
-
-  }
+//  def update(): Unit = {
+//    val updateFuture = ItemDAO.update()
+//    updateFuture.onComplete{
+//      case Success(value) =>
+//        print("\n")
+//        value.foreach(println)
+//      case Failure(error) =>
+//        error.printStackTrace()
+//    }
+//  }
 
 }
